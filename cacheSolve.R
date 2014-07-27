bedd8c56bc653cbb8848af14ba97eb7759165c20
MattGarner <matt.j.garner@gmail.com>
2014-07-27 15:31:24 +0100

    Improved structure and comments on .R files for project

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,23 @@
+##This function computes the inverse of the special "matrix" returned by makeCacheMatrix. 
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cacheSolve should retrieve the inverse from the cache.
+
 cacheSolve <- function(x, ...) {
+        #Get the cached value for inverse matrix which is stored in makeCacheMatrix object x
         i <- x$getinv()
+        
+        #If a cached result was found, then return it, and acknowledge that it is a cached value
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
         }
+        
+        #Obtain matrix data from object x
         data <- x$get()
+        #Calculate the inverse of the matrix
         i <- solve(data, ...)
+        #Cache the result of the calculation within object x
         x$setinv(i)
+        #return the newly calculated inverse matrix i
         i
 }
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,27 +1,30 @@
+##This function creates a special "matrix" object that can cache its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
 	  #i is inverse of x, is unknown when object created
 	  i <- NULL
 	 
 	  #The set function assigns a new value to x (the matrix). If a new value
 	  #of x is set then the value of i is set to NULL, since previously calculated values
-        #will be incorrect if new x != old x
-        set <- function(y) {
-                x <<- y
-                i <<- NULL
-        }
+    #will be incorrect if the new matrix values differ from the old ones
+    set <- function(y) {
+            x <<- y
+            i <<- NULL
+    }
 	  
-        #get returns the value of x within the object
-        get <- function() x
+    #get returns the value of x within the object
+    get <- function() x
         
-        #setinv is called by cacheSolve.R to assign it's result to i, the cached inverse
-	  setinv <- function(inv) i <<- inv
+    #setinv is called by cacheSolve.R to assign it's result to i, the cached inverse
+    setinv <- function(inv) i <<- inv
         
 	  #getinv returns the inverse of x (will be NULL before cacheSolve is called on the 
-        #makeCacheMatrix object)
-	  getinv <- function() i
+    #makeCacheMatrix object)
+    getinv <- function() i
         
 	  #Items to display when a makeCacheMatrix object is called
-	  list(set = set, get = get,
-             setinv = setinv,
-             getinv = getinv)
+	  list(set = set,
+         get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
