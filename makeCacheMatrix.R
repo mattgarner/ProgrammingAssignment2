00d159ffa4de8a3db6f7fdac556c0619b511e5cd
MattGarner <matt.j.garner@gmail.com>
2014-07-27 14:45:15 +0100

    Created makeCacheMatrix.R and cacheSolve.R

---
+++ makeCacheMatrix.R
@@ -0,0 +1,27 @@
+makeCacheMatrix <- function(x = matrix()) {
+	  #i is inverse of x, is unknown when object created
+	  i <- NULL
+	 
+	  #The set function assigns a new value to x (the matrix). If a new value
+	  #of x is set then the value of i is set to NULL, since previously calculated values
+        #will be incorrect if new x != old x
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+	  
+        #get returns the value of x within the object
+        get <- function() x
+        
+        #setinv is called by cacheSolve.R to assign it's result to i, the cached inverse
+	  setinv <- function(inv) i <<- inv
+        
+	  #getinv returns the inverse of x (will be NULL before cacheSolve is called on the 
+        #makeCacheMatrix object)
+	  getinv <- function() i
+        
+	  #Items to display when a makeCacheMatrix object is called
+	  list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+}
